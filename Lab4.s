1f7e53ee5435b3d46e413aab77d646e1c3f05e76
Bilotti <m260516@academy.usna.edu>
2024-09-12 15:03:15 -0400

    Andrew is a sigma

--- Lab4.s
+++ Lab4.s
@@ -3,6 +3,7 @@
 		ALIGN
 		ENTRY
 __main	PROC
+		; Group: Andrew Bilotti & Joshua Wynn
 		; Enable GPIO Clock (Power on)
 		MOVW 	R2, #0x104C
 		MOVT 	R2, #0x4002
@@ -33,16 +34,36 @@ loop	LDR 	R3, [R2, #0x10] ; address of GPIOC_IDR (shows if input pins are on or 
 		; TODO: Determine if you should jump to button1, button2, or show
 		; based on the value in R2
 		
+		;IDR tells me if output pins are on and off too, which is weird.
+		;So we need to reset all but bits 4 and 5 (PC4 and PC5)
+		;0000 0000 00XX 0000
+		BFC R3, #0, #4
+		BFC R3, #6, #10
 		
+		; PC_4 for increment and PC_5 for decrement
 		
+		CMP R3, #0x30
+		BEQ show ; Buttons arent pressed
+		CMP R3, #0x20 ; PC4 pressed
+		BEQ button1
+		CMP R3, #0x10 ; PC5 pressed
+		BEQ button2
 		
 		B		loop
 		
+		
+		
 button1	; TODO: Increment R0 and branch to the next appropriate label
+		CMP R0, #0xFF
+		BEQ loop
+		ADD R0, R0, #1
+		B		waitbutton
 		
-
 button2	; TODO: Decrement R0 and branch to the next appropriate label
-		
+		CMP R0, #0x00
+		BEQ loop
+		SUB R0, R0, #1
+		B		waitbutton
 		
 show	; display N on LEDs after updating value
 		MOV		R3, R0
@@ -50,6 +71,15 @@ show	; display N on LEDs after updating value
 		LSR 	R3, R3, #4
 		BFI 	R1, R3, #8, #4
 		STR 	R1, [R2, #0x14]
+		B loop
+		
+waitbutton
+		LDR R3, [R2, #0x10]
+		BFC R3, #0, #4
+		BFC R3, #6, #10
+		CMP R3, #0x30
+		BEQ show
+		B waitbutton
 		
 		; Your code goes above this line
 		ENDP
--- Lab4_start.s
+++ Lab4_start.s
@@ -13,12 +13,14 @@ __main	PROC
 		; Set PC0 as output, PC4 as input
 		MOVW 	R2, #0x0800
 		MOVT 	R2, #0x4800 ; Address of GPIOC_MODER
-
+		; 0000 0000 0000 0000 0000 1000 0000 0000
+		;                      7 6  5 4  3 2  1 0
 		LDR 	R0, [R2]
 		MOVW 	R0, #0xFCFD
 		MOVT 	R0, #0xFFFF
 		STR 	R0, [R2]
-		
+		; The default state of an input GPIO pin is "floating," meaning the pin is not connected to a voltage source or ground, so the logic is not determinable
+		; There is an internal pull up resistor on PC4, and we must enable it so that when it is not connected to ground, it is HI by default :)
 		; set internal pull-up resistor on PC4
 		LDR		R3, [R2, #0x0C] ; address of GPIOC_PUPDR
 		BIC		R3, #0x200
@@ -27,8 +29,18 @@ __main	PROC
 		
 		; Now, we continuously loop, checking for inputs on the button
 loop	LDR 	R3, [R2, #0x10] ; R2 is the value of GPIOC_IDR (shows if input pins are on or off)	
+		; 0000 0000 0001 0000
+		;Set PC0 as ON if input
+		;Easiest way would be bitwise shift right 4 bits then negate
+		LSR		R3, R3, #4 ; Shift to the right 4, where odr bit is for led
+		; 0000 0000 0000 0001
+		; If on 1-0 = 1 if off 1 - 1 = 0!
+		; 0000 0000 0000 0001
+		RSB		R3, R3, #0x01
 		
+		STR		R3, [R2, #0x14]
 		B		loop
+		;PC4 active-low logic, so push means its gonna give us a zero
 		
 		; Your code goes above this line
 		ENDP
